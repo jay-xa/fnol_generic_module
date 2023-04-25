fae34945cfac1a968180a0b095d68149a7907a1c
MungaraJay <mungara.j@xagroup.com>
2023-04-25 18:21:31 +0530

    Initial Commit

---
+++ lib/fnol_generic_module.dart
@@ -0,0 +1,3 @@
+library fnol_module;
+
+export 'package:fnol_generic_module/fnol_screen.dart';
---
+++ lib/fnol_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class FNOLScreen extends StatefulWidget {
+  const FNOLScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FNOLScreen> createState() => _FNOLScreenState();
+}
+
+class _FNOLScreenState extends State<FNOLScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('FNOL Screen');
+  }
+}
