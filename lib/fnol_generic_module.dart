0fc18aaa94ea55a24192414c0a9515ad1bd818e6
MungaraJay <mungara.j@xagroup.com>
2023-04-27 17:45:52 +0530

    Sample Update

--- lib/fnol_generic_module.dart
+++ lib/fnol_generic_module.dart
@@ -1,3 +1,17 @@
 library fnol_module;
 
 export 'package:fnol_generic_module/fnol_screen.dart';
+
+class FnolGenericModule {
+  String? accessToken;
+
+  static FnolGenericModule? genericModule;
+
+  void initSetup({String? token}) {
+    accessToken = token;
+  }
+
+  static FnolGenericModule getInstance() {
+    return genericModule ??= FnolGenericModule();
+  }
+}
